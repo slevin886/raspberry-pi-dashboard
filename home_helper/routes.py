d26029d578f11185cb6a82f8d952a05fe775c67f
Scott Levin <slevin886@gmail.com>
2019-10-25 08:20:57 -0400

    added get_historic_events endpoint

--- home_helper/routes.py
+++ home_helper/routes.py
@@ -3,6 +3,13 @@ import pyowm
 from datetime import datetime
 import os
 import requests
+from bs4 import BeautifulSoup
+from random import sample
+import wikipedia
+
+
+# TODO: implement JS for get_historic_events
+# TODO: set init js methods as async
 
 home_helper = Blueprint("home_helper", __name__)
 open_weather_api_key = os.environ.get('OPEN_ENV_KEY')
@@ -11,21 +18,32 @@ OWM = pyowm.OWM(open_weather_api_key)
 # Change for your needs
 USER_NAME = 'Ariel + Scott + Gracie'  # for welcome message
 BREED = ['retriever', 'golden']  # dog breed image to see
-CITY_COORDS = {'lat': 42.313250, 'lon': -71.114173}  # get your city's latitude/longitude
+CITY_ID = 4930956  # get your city's OWM ID
+
 
 # VIEWS
-
-
 @home_helper.route('/', methods=['GET'])
 def home():
     return render_template('home.html', user_name=USER_NAME)
 
 
-# API
+# API Endpoints
+
+@home_helper.route('/get_historic_events', methods=['GET'])
+def get_historic_events():
+    date = datetime.now()
+    date = date.strftime('%B') + ' ' + str(date.day)
+    page = wikipedia.WikipediaPage(date)
+    soup = BeautifulSoup(page.html())
+    events = soup.find("span", id="Events").find_next("ul").find_all("li")
+    events = [i.text for i in events]
+    events = sample([i for i in events if len(i) < 108], 3)  # limit event text length bc/ screen size
+    return jsonify({'events': events}), 200
+
+
 @home_helper.route('/get_weather', methods=['GET'])
 def get_weather():
-    weather = OWM.weather_at_coords(CITY_COORDS['lat'], CITY_COORDS['lon']).get_weather()
-    print(weather)
+    weather = OWM.daily_forecast_at_id(CITY_ID).get_weather()
     resp = dict()
     resp['temperature'] = weather.get_temperature(unit='fahrenheit')
     resp['wind'] = str(weather.get_wind()['speed'])  # meters/sec
@@ -34,7 +52,6 @@ def get_weather():
     hour = str(sunset.hour - 12) if sunset.hour > 12 else str(sunset.hour)
     minute = '0' + str(sunset.minute) if sunset.minute < 10 else str(sunset.minute)
     resp['sunset'] = hour + ':' + minute + 'pm'
-    print(resp)
     return jsonify(resp), 200
 
 
